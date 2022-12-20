37a4bd3ceeebaa044d1deb61fbb409a067a81209
Parabeac-Nest <admin@parabeac.com>
2022-12-20 04:33:55 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/leg_theme.g.dart
@@ -0,0 +1,22 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';import 'package:leg/theme/leg_text_styles.g.dart';
+import 'package:leg/theme/leg_colors.g.dart';
+
+class LegTheme {
+
+static const TextTheme textTheme = TextTheme(headline1: LegTextStyles.headline1,headline2: LegTextStyles.headline2,headline3: LegTextStyles.headline3,headline4: LegTextStyles.headline4,headline5: LegTextStyles.headline5,headline6: LegTextStyles.headline6,subtitle1: LegTextStyles.subtitle1,subtitle2: LegTextStyles.subtitle2,bodyText1: LegTextStyles.bodyText1,bodyText2: LegTextStyles.bodyText2,button: LegTextStyles.button,caption: LegTextStyles.caption,overline: LegTextStyles.overline,);
+
+static const ColorScheme light = ColorScheme.light(onPrimary: LegColors.lightOnPrimary,primary: LegColors.lightPrimary,primaryContainer: LegColors.lightPrimaryContainer,secondary: LegColors.lightSecondary,onSecondary: LegColors.lightOnSecondary,onPrimaryContainer: LegColors.lightOnPrimaryContainer,secondaryContainer: LegColors.lightSecondaryContainer,onSecondaryContainer: LegColors.lightOnSecondaryContainer,onTertiary: LegColors.lightOnTertiary,tertiary: LegColors.lightTertiary,tertiaryContainer: LegColors.lightTertiaryContainer,onTertiaryContainer: LegColors.lightOnTertiaryContainer,error: LegColors.lightError,onError: LegColors.lightOnError,errorContainer: LegColors.lightErrorContainer,onErrorContainer: LegColors.lightOnErrorContainer,background: LegColors.lightBackground,onBackground: LegColors.lightOnBackground,surface: LegColors.lightSurface,onSurface: LegColors.lightOnSurface,surfaceVariant: LegColors.lightSurfaceVariant,onSurfaceVariant: LegColors.lightOnSurfaceVariant,outline: LegColors.lightOutline,inverseSurface: LegColors.lightInverseSurface,onInverseSurface: LegColors.lightOnInverseSurface,inversePrimary: LegColors.lightInversePrimary,surfaceTint: LegColors.lightSurfaceTint);
+
+static  ThemeData themeDataLight = ThemeData(textTheme: textTheme, colorScheme: light,);
+
+static const ColorScheme dark = ColorScheme.dark(primary: LegColors.darkPrimary,onPrimary: LegColors.darkOnPrimary,primaryContainer: LegColors.darkPrimaryContainer,onPrimaryContainer: LegColors.darkOnPrimaryContainer,secondary: LegColors.darkSecondary,onSecondary: LegColors.darkOnSecondary,secondaryContainer: LegColors.darkSecondaryContainer,onSecondaryContainer: LegColors.darkOnSecondaryContainer,tertiary: LegColors.darkTertiary,onTertiary: LegColors.darkOnTertiary,tertiaryContainer: LegColors.darkTertiaryContainer,onTertiaryContainer: LegColors.darkOnTertiaryContainer,error: LegColors.darkError,onError: LegColors.darkOnError,errorContainer: LegColors.darkErrorContainer,onErrorContainer: LegColors.darkOnErrorContainer,background: LegColors.darkBackground,onBackground: LegColors.darkOnBackground,surface: LegColors.darkSurface,onSurface: LegColors.darkOnSurface,surfaceVariant: LegColors.darkSurfaceVariant,outline: LegColors.darkOutline,inverseSurface: LegColors.darkInverseSurface,inversePrimary: LegColors.darkInversePrimary,surfaceTint: LegColors.darkSurfaceTint,onSurfaceVariant: LegColors.darkOnSurfaceVariant,onInverseSurface: LegColors.darkOnInverseSurface);
+
+static  ThemeData themeDataDark = ThemeData(textTheme: textTheme, colorScheme: dark,);
+}
